99ba61f0b88997ec1c667e0869c9406e7b235cdb
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2020-08-03 16:36:41 -0400

    Sort imports

--- java/microphone/toggle/Main.java
+++ java/microphone/toggle/Main.java
@@ -1,16 +1,16 @@
 package microphone.toggle;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.Context;
 import android.media.AudioManager;
+import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.View;
 
 import microphone.toggle.R;
 
